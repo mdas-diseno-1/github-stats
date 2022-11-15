dbb536c8ac63946c324820e8507e666e157a5dd9
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2022-11-15 23:40:44 +0100

    feat: add initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PullRequestsModule } from './pull-requests/pull-requests.module';
+import { PullRequestReviewsModule } from './pull-request-reviews/pull-request-reviews.module';
+import { ReviewCommentsModule } from './review-comments/review-comments.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PullRequestsModule,
+    PullRequestReviewsModule,
+    ReviewCommentsModule,
+    MetricsModule,
+    UsersModule,
+    OrganizationsModule,
+    TeamsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
