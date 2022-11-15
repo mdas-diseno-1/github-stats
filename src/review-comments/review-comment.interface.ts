3973377220d9bd4e3be2d5c9702d5bbf82beeb36
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2022-11-15 23:39:50 +0100

    feat: add review-comments resource and basic model

---
+++ src/review-comments/review-comment.interface.ts
@@ -0,0 +1,10 @@
+export interface ReviewCommentInterface {
+  id: bigint;
+  user_id: bigint;
+  pull_request_id: bigint;
+  pull_request_review_id;
+  bigint;
+  body: string;
+  created_at: string;
+  updated_at: string;
+}
