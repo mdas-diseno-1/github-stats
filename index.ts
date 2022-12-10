19036e424534651ebb941946979ade608da216ef
Santiago Ibarzabal <santiagoibarzabal91@gmail.com>
2022-12-10 21:41:46 +0100

    feat: add type orm

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-import getPullRequestServices from "./src/application/services/getPullRequest.services";
-import { PullRuquestExecuted } from './src/application/user_cases/getPullRequests';
+import {PullRuquestExecuted} from "./src/Application/user_cases/getPullRequests";
 const prompt = require('prompt-sync')({ sigint: true });
+import { AppDataSource } from "./data-source"
 import * as yargs from 'yargs'
 import { resolve } from "path";
+
+AppDataSource.initialize().then(async () => {
+
+}).catch(error => console.log(error))
+
 
 const name = prompt('What is your name? ');
 const month = prompt('What month (YYYY-MM): ');
 const pullRuquestExecuted = new PullRuquestExecuted(name, month);
 pullRuquestExecuted.getPrExecuteCount().then((value) => {
     console.log(value);
-})
+});
 
 // const { argv } = yargs.option("userName", { describe: "GitHub user name" })
 // const userName = argv["_"];
