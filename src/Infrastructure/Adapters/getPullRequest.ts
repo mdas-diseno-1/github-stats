2eea8a281d9886576e8b43375512ee0a02eb43f7
Santiago Ibarzabal <santiagoibarzabal91@gmail.com>
2022-12-11 16:50:47 +0100

    fix: add .env.example

--- src/Infrastructure/Adapters/getPullRequest.ts
+++ src/Infrastructure/Adapters/getPullRequest.ts
@@ -3,14 +3,15 @@ import { string } from "yargs";
 
 const axios = require('axios');
 let data = '';
+const API_ENDPOINT = 'https://api.github.com';
 
 export const syncGetPullRequestsExecuted = async (userName: string, month: string): Promise<string> => {
     console.log(` user name: ${userName} , month: ${month}`);
     const headers = {
-        'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+        'Authorization': 'Bearer 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'
     };
     const config = {headers: headers}
-    return axios.get(`${process.env.GITHUB_API}/search/issues?q=type:pr+author:${userName}+created:${month}`, config)
+    return axios.get(`${API_ENDPOINT}/search/issues?q=type:pr+author:${userName}+created:${month}`, config)
         .then((res) => res.data.count);
 }
 
