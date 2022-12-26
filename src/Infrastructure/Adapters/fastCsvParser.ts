c0703713b9ff846030bbf3fa91421b30473c8164
l.gutierrez <l.gutierrez@students.salle.url.edu>
2022-12-26 15:10:28 +0100

    Arreglada importación del contrato de file parser

--- src/Infrastructure/Adapters/fastCsvParser.ts
+++ src/Infrastructure/Adapters/fastCsvParser.ts
@@ -1,13 +1,16 @@
 
-import { IFileParserInterface } from './FileParser'
+import {IFileParserInterface} from "../../Domain/FileParser";
 const fs = require("fs");
 const fastCsv = require("fast-csv");
 export class fastCsvParser implements IFileParserInterface{
     
     
     public readFile (filePath: string): Promise<boolean>{
+        return Promise.resolve(false);
+    }
 
-        return
+    writeFile(data: any[]): Promise<boolean> {
+        return Promise.resolve(false);
     }
 
 }
